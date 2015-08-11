7a4d8585e6d89c2df0128559b80672cf9d0777c6
Mandeep Ahlawat <mandeep052@gmail.com>
2015-08-11 11:57:38 +0530

    use script from example and customized to filter by price range

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -3,32 +3,207 @@ module Searchable
 
   included do
     include Elasticsearch::Model
-    include Elasticsearch::Model::Callbacks
 
-    mappings do
+    # Customize the index name
+    #
+    index_name [Rails.application.engine_name, Rails.env].join('_')
+
+    # Set up index configuration and mapping
+    #
+    settings index: { number_of_shards: 1, number_of_replicas: 0 } do
+      mapping do
+        indexes :name, type: 'multi_field' do
+          indexes :name,     analyzer: 'snowball'
+          indexes :tokenized, analyzer: 'simple'
+        end
+
+        indexes :about, type: 'multi_field' do
+          indexes :about,   analyzer: 'snowball'
+          indexes :tokenized, analyzer: 'simple'
+        end
+
+        indexes :gender, type: 'multi_field'
+        indexes :price, type: 'multi_field'
+        indexes :review_count, type: 'multi_field'
+        indexes :avg_rating, type: 'multi_field'
+
+        indexes :openings do
+          indexes :start_time, type: 'date'
+          indexes :end_time, type: 'date'
+          indexes :status, type: 'keyword'
+          indexes :session_time_in_sec, type: 'multi_field'
+        end
+
+        indexes :specializations, analyzer: 'keyword'
+      end
     end
 
-    def self.search(query)
-      __elasticsearch__.search(
-        {
-          query: {
-            multi_match: {
-              query: query,
-              fields: ['name^1', 'about^2', 'specializations.name^3']
+    # Set up callbacks for updating the index on model changes
+    #
+    after_commit lambda { Indexer.perform_async(:index,  self.class.to_s, self.id) }, on: :create
+    after_commit lambda { Indexer.perform_async(:update, self.class.to_s, self.id) }, on: :update
+    after_commit lambda { Indexer.perform_async(:delete, self.class.to_s, self.id) }, on: :destroy
+    after_touch  lambda { Indexer.perform_async(:update, self.class.to_s, self.id) }
+
+    # Customize the JSON serialization for Elasticsearch
+    #
+    def as_indexed_json(options={})
+      hash = self.as_json(
+        include: { 
+                   openings:   { only: [:start_time, :end_time, :session_time_in_sec, :status] }
+                 })
+      hash['specializations'] = self.specializations.map(&:name)
+      hash
+    end
+
+    # Search in name and about fields for `query`, include highlights in response
+    #
+    # @param query [String] The user query
+    # @return [Elasticsearch::Model::Response::Response]
+    #
+    def self.search(query, options={})
+
+      # Prefill and set the filters (top-level `filter` and `facet_filter` elements)
+      #
+      __set_filters = lambda do |key, f|
+        @search_definition[:filter][:and] ||= []
+        @search_definition[:filter][:and]  |= [f]
+
+        # @search_definition[:facets][key.to_sym][:facet_filter][:and] ||= []
+        # @search_definition[:facets][key.to_sym][:facet_filter][:and]  |= [f]
+      end
+
+      @search_definition = {
+        query: {},
+
+        highlight: {
+          pre_tags: ['<em class="label label-highlight">'],
+          post_tags: ['</em>'],
+          fields: {
+            name:    { number_of_fragments: 0 },
+            about:  { fragment_size: 50 }
+          }
+        },
+
+        filter: {},
+
+        # facets: {
+        #   specializations: {
+        #     terms: {
+        #       field: 'specializations'
+        #     },
+        #     facet_filter: {}
+        #   },
+        #   openings: {
+        #     terms: {
+        #       field: 'openings.start_time'
+        #     },
+        #     facet_filter: {}
+        #   },
+        #   published: {
+        #     date_histogram: {
+        #       field: 'published_on',
+        #       interval: 'week'
+        #     },
+        #     facet_filter: {}
+        #   }
+        # }
+      }
+
+      unless query.blank?
+        @search_definition[:query] = {
+          bool: {
+            should: [
+              { multi_match: {
+                  query: query,
+                  fields: ['name^1', 'about^2', 'specializations.name^3, gender, price, avg_rating'],
+                  operator: 'and'
+                }
+              }
+            ]
+          }
+        }
+      else
+        @search_definition[:query] = { match_all: {} }
+        # @search_definition[:sort]  = { published_on: 'desc' }
+      end
+
+      if options[:price]
+        f = {
+          range: {
+            price: {
+              gte: options[:price][:start_val],
+              lte: options[:price][:end_val]
             }
           }
         }
-      )
-    end
+        __set_filters.(:price, f)
+      end
 
-    def as_indexed_json(options={})
-      as_json(
-        only: [:name, :about],
-        include: {
-          specializations: {only: [:name, :id]},
-          openings: {only: [:start_time, :end_time, :session_time_in_sec, :status, :id]}
+
+      # if options[:author]
+      #   f = { term: { 'authors.full_name.raw' => options[:author] } }
+
+      #   __set_filters.(:categories, f)
+      #   __set_filters.(:published, f)
+      # end
+
+      # if options[:published_week]
+      #   f = {
+      #     range: {
+      #       published_on: {
+      #         gte: options[:published_week],
+      #         lte: "#{options[:published_week]}||+1w"
+      #       }
+      #     }
+      #   }
+
+      #   __set_filters.(:categories, f)
+      #   __set_filters.(:authors, f)
+      # end
+
+      # if query.present? && options[:comments]
+      #   @search_definition[:query][:bool][:should] ||= []
+      #   @search_definition[:query][:bool][:should] << {
+      #     nested: {
+      #       path: 'comments',
+      #       query: {
+      #         multi_match: {
+      #           query: query,
+      #           fields: ['body'],
+      #           operator: 'and'
+      #         }
+      #       }
+      #     }
+      #   }
+      #   @search_definition[:highlight][:fields].update 'comments.body' => { fragment_size: 50 }
+      # end
+
+      # if options[:sort]
+      #   @search_definition[:sort]  = { options[:sort] => 'desc' }
+      #   @search_definition[:track_scores] = true
+      # end
+
+      unless query.blank?
+        @search_definition[:suggest] = {
+          text: query,
+          suggest_title: {
+            term: {
+              field: 'name.tokenized',
+              suggest_mode: 'always'
+            }
+          },
+          suggest_body: {
+            term: {
+              field: 'about.tokenized',
+              suggest_mode: 'always'
+            }
+          }
         }
-      )
+      end
+
+      debugger
+      __elasticsearch__.search(@search_definition)
     end
   end
 end
