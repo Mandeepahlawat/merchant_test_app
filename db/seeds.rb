87b6960d9c45352fbccf3725e24006d255056398
Mandeep Ahlawat <mandeep052@gmail.com>
2015-08-11 17:21:26 +0530

    randomize seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Merchant.create(name: "Vipin", email: "abc@email.com", status: 1, about: "This is a seeded merchant.", gender: 1, price: 10.2, avg_rating: 2, review_count: 2)
-Merchant.create(name: "Sample User", email: "abc1@email.com", status: 1, about: "This is a sample seeded merchant.", gender: 0, price: 50, avg_rating: 3, review_count: 5)
-Merchant.create(name: "Sample User 1", email: "abc2@email.com", status: 1, about: "This is another sample seeded merchant.", gender: 2, price: 30, avg_rating: 5, review_count: 1)
+Merchant.create(name: "Sample User", email: "abc@email.com", status: 1, about: "This is a seeded merchant.", gender: 1, price: 10.2, avg_rating: 2, review_count: 2)
+Merchant.create(name: "Sample User 1", email: "abc1@email.com", status: 1, about: "This is a sample seeded merchant.", gender: 0, price: 50, avg_rating: 3, review_count: 5)
+Merchant.create(name: "Sample User 2", email: "abc2@email.com", status: 1, about: "This is another sample seeded merchant.", gender: 2, price: 30, avg_rating: 5, review_count: 1)
 
 ["Massage", "Waxing", "Hair Cut", "Yoga"].each {|text| Specialization.create(name: text)}
 
